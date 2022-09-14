cda26d94ca5faceefe1fd4b30d026da61d416ba4
Eric Larson <eclarson@smu.edu>
2022-09-14 10:04:42 -0500

    Adding some HCI elements as promised in class

--- Digits/GuessViewController.swift
+++ Digits/GuessViewController.swift
@@ -8,58 +8,100 @@
 import UIKit
 
 class GuessViewController: UIViewController, UITextFieldDelegate {
-
+    
+    // MARK: UI Outlets
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var largeFeedbackLabel: UILabel!
+    @IBOutlet weak var textFieldGuess: UITextField!
+    @IBOutlet weak var guessButton: UIButton!
+    
+    // MARK: Model
+    var guessModel:GuessModel = GuessModel()
+    
+    // MARK: View Hierarchy Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         self.textFieldGuess.delegate = self
+        self.textFieldGuess.becomeFirstResponder()
     }
-    
-    @IBOutlet weak var feedbackLabel: UILabel!
-    
-    @IBOutlet weak var largeFeedbackLabel: UILabel!
-    
-    @IBOutlet weak var textFieldGuess: UITextField!
-    
-    @IBOutlet weak var guessButton: UIButton!
-    var guessModel:GuessModel = GuessModel()
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.textFieldGuess.resignFirstResponder()
     }
     
+    
+    // MARK: UI Actions
     @IBAction func makeGuess(_ sender: Any) {
         
         if let guess = Int(self.textFieldGuess.text!),
         guess>0,
         guess<=100{
-            var textToAdd = "\(guess)"
-            switch self.guessModel.makeAGuess(guess) {
-            case GuessValueLower:
-                self.feedbackLabel.text = "Lower!"
-                textToAdd += "<, "
-            case GuessValueHigher:
-                self.feedbackLabel.text = "Higher!"
-                textToAdd += ">, "
-            case GuessValueCorrect:
-                self.feedbackLabel.text = "Correct!"
-                textToAdd += " Right!! "
-                self.guessButton.isEnabled = false
-                
-            default:
-                print("never be here")
+            // this is a valid guess, send to guessing
+            DispatchQueue.main.async {
+                self.interpret(guess: guess)
             }
             
-            self.textFieldGuess.text = ""
-            self.largeFeedbackLabel.text! += textToAdd
-            
-            
+        }else{
+            // this is a valid guess, send to guessing
+            DispatchQueue.main.async {
+                self.feedbackLabel.text = "Please enter a valid integer 1-100"
+            }
         }
     }
     
     @IBAction func tapGestureRecognized(_ sender: Any) {
         self.textFieldGuess.resignFirstResponder()
+    }
+    
+    func interpret(guess:Int){
+        // setup some animation for labels
+        var anim = UIView.AnimationOptions.transitionCrossDissolve
+        var duration = 0.33
+        
+        var textToAdd = "\(guess)"
+        var textForLabel = ""
+        switch self.guessModel.makeAGuess(guess) {
+            case GuessValueLower:
+                textForLabel = "Lower!"
+                textToAdd += "<, "
+            case GuessValueHigher:
+                textForLabel = "Higher!"
+                textToAdd += ">, "
+            case GuessValueCorrect:
+                textForLabel = "Correct!"
+                textToAdd += " Correct!! "
+                self.guessButton.isEnabled = false
+                anim = .transitionFlipFromRight
+                duration = 3.0
+                self.textFieldGuess.resignFirstResponder()
+                
+            default:
+                print("never be here")
+        }
+        
+        
+        // update the user feedback
+        UIView.transition(with: self.largeFeedbackLabel,
+              duration: duration,
+              options: anim,
+              animations: {
+                    self.largeFeedbackLabel.text! += textToAdd
+                          },
+              completion: nil)
+        
+        
+        // update the user feedback
+        UIView.transition(with: self.feedbackLabel,
+              duration: duration,
+              options: anim,
+              animations: {
+            self.feedbackLabel.text = textForLabel
+                          },
+              completion: nil)
+        
+        self.textFieldGuess.text = ""
     }
 }
